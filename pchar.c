75460f132ee0f2d51143df9512876dc5625aea5b
josephorokpo <josephorokpo@gmail.com>
2023-10-19 13:53:42 -0700

    pchar.c modified

--- pchar.c
+++ pchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "monty.h"
 
 /**
  * pchar - Prints the character at the top of the stack.
@@ -7,13 +7,15 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
+	int value;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	int value = (*stack)->n;
+	value = (*stack)->n;
 
 	if (value < 0 || value > 127)
 	{
